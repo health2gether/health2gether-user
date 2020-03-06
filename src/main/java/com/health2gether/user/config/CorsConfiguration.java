f4322237dbf762c1a767d263a63bf67ba903455b
Flavio Oliveira <flaoliveira@uolinc.com>
2020-03-06 16:14:10 -0300

    commit

---
+++ src/main/java/com/health2gether/user/config/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.health2gether.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("!dev")
+public class CorsConfiguration
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "http://health2gether.s3-website-us-east-1.amazonaws.com");
+            }
+        };
+    }
+}
